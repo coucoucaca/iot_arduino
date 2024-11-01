199b9e646651ba35a87b908993c84f823e93207c
Akiko McQuerrey <78618012+coucoucaca@users.noreply.github.com>
2024-11-01 11:23:52 -0400

    Add files via upload

--- smart_fridge_part1.ino
+++ smart_fridge_part1.ino
@@ -1,17 +1,19 @@
 //assign pins and variables
-const int switch_pin = D1;//  D1 pin to read the SIG from the switch
-
+const int switch_pin = D1; //  D1 pin to read the SIG from the switch
 const int led_pin = D2;
-int switch_value;
 
 const int potentiometer_pin = A0; // A0 pin ( analog) for potatiometer SIG
-int potValue;
 
-//imitialize temperature
+//variables for values
+int switch_value;//0 or 1
+int potValue; // analog val ( 0 - 1023 or mapped range)
+int brightness_val; // analog val ( 0 - 1023 or mapped range)
+
+//initialize temperature
 int minTemp = -2;
 int maxTemp = 6;
 
-int brightness_val;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -34,16 +36,15 @@ void loop() {
     delay(200);
     Serial.println(fridgeTemperature());
     delay(200);
-    // Serial.println(analogRead(potentiometer_pin));
-    // delay(200);
-    // Serial.println(brighteness());
-    
-    delay(200);
+
   }  else  {
     
     Serial.println("Fridge OFF");
-    potValue=analogRead(potentiometer_pin);
-    brightness_val = map(potValue,0,1023,0,255);
+    potValue= analogRead(potentiometer_pin);
+    brightness_val = map(potValue,0,1023,0,255);//mapped to LED range
+    Serial.println(" Brightness  ");
+    Serial.println(brightness_val);
+    //assign the potentiometer
     analogWrite(led_pin, brightness_val);
 
   }
@@ -67,8 +68,7 @@ int fridgeTemperature() {
 
   potValue = analogRead(potentiometer_pin);
   //map to the voltage range(0- 1023 which is the resolution of the MC) to min and max temp
-
-  // potValue= map(potValue, 0, 1023, minTemp, maxTemp); //5 args
+  potValue= map(potValue, 0, 1023, minTemp, maxTemp); //5 args
   return potValue;
 
 }
